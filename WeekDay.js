973ee2a909eaea05f5f679888463be4e40131f4f
Siddhant <santoshshrivastava39@gmail.com>
2022-05-01 15:14:00 +0530

    Display the week day

---
+++ WeekDay.js
@@ -0,0 +1,29 @@
+let day = Math.floor(Math.random()*7)+1;
+
+console.log("Week : "+day);
+
+switch(day)
+{
+        
+        case 1:
+        console.log("Its Monday")
+        break;
+        case 2:
+        console.log("Its Tuesday")
+        break;   
+        case 3:
+        console.log("Its Wednesday")
+        break;
+        case 4:
+        console.log("Its Thursday")
+        break;
+        case 5:
+        console.log("Its Friday")
+        break;
+        case 6:
+        console.log("Its Saturday")
+        break;
+        case 7:
+        console.log("Its Sunday")
+        break;
+}
