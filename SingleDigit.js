8f28902640fca1ca6e6619eba70ef1d2b77c591f
Siddhant <santoshshrivastava39@gmail.com>
2022-05-01 15:10:51 +0530

    Read single digit no and write in words

---
+++ SingleDigit.js
@@ -0,0 +1,42 @@
+let singledigit = Math.floor(Math.random()*10);
+
+console.log("Number : "+singledigit);
+
+switch(singledigit)
+{
+        case 0:
+        console.log("In words-Zero")
+        break;
+        case 1:
+        console.log("In words-One")
+        break;
+        case 2:
+        console.log("In words-Two")
+        break;   
+        case 3:
+        console.log("In words-Three")
+        break;
+        case 4:
+        console.log("In words-Four")
+        break;
+        case 5:
+        console.log("In words-Five")
+        break;
+        case 6:
+        console.log("In words-Six")
+        break;
+        case 7:
+        console.log("In words-Seven")
+        break;
+    
+        case 8:
+        console.log("In words-Eight")
+        break;
+        case 9:
+        console.log("In words-Nine")
+        break;
+
+
+}
+ 
+
